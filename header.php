2b086232d870acb667403242bc56ff1fa7638792
Innar Hallik <innar.hallik@gmail.com>
2015-02-17 20:41:01 +0200

    small fixes

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
+<!doctype html>
 <html>
     <head>
         <title>Octo-poll</title>
         <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="utf-8">
     </head>
 <body>
--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <div id="static">
-	<br>
-	<br>
-	<img style="margin:0px auto;display:block" src="icon/name.png" alt="Octopoll"/>
-	<br>
-	<a href="https://github.com/Kixile/octo-poll">Visit our Git</a>
-	<br>
-	<h2>Team</h2>
-	<p>Karl-Sander Häidma</p>
-	<p>Innar Hallik</p>
-	<p>Jarmo-Hendrik Hints</p>
+    <br>
+    <br>
+    <img style="margin:0px auto;display:block" src="icon/name.png" alt="Octopoll"/>
+    <br>
+    <a href="https://github.com/Kixile/octo-poll">Visit our Git</a>
+    <br>
+    <h2>Team</h2>
+    <p>Karl-Sander HÃ¤idma</p>
+    <p>Innar Hallik</p>
+    <p>Jarmo-Hendrik Hints</p>
 </div>
