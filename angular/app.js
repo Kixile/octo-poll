25a50afa5a2431b419d7a43f56dc290e84f77c98
Innar Hallik <innar.hallik@gmail.com>
2015-02-22 21:01:09 +0200

    polls list + search later need to connect with database

--- angular/app.js
+++ angular/app.js
@@ -16,18 +16,34 @@
         };
     });
 
-    var gems = [
+    app.controller('PollsController', function(){
+        this.polls = polls;
+    });
+
+    var polls = [
         {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: '. . .',
-            canPurchase: true
+            question: 'How do we get end-users?',
+            author: 'admin',
+            description: 'What are your suggestions on this topic.',
+            type: 1,
+            date: 1288323623006,
+            options: []
         },
         {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: '. . .',
-            canPurchase: true
+            question: 'How we become popular?',
+            author: 'admin',
+            description: 'Pick strategy what will help us the most.',
+            type: 2,
+            date: 1288323623006,
+            options: ["adds","spam mail","find sponsors"]
+        },
+        {
+            question: 'What other poll creation tools you use?',
+            author: 'admin',
+            description: 'Select one or more options that you use.',
+            type: 3,
+            date: 1288323623006,
+            options: ['poll-maker','asypolls','pollcode','strawpoll','surveymonkey']
         }
     ];
 })();
