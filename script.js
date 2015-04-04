b531f1998fbf2f6cba5eee04406c7ea523d546e2
Jarmo-Hendrik Hints <malbidion@gmail.com>
2015-04-04 22:00:39 +0300

    UI overhaul, fixed angular and bootstrap

---
+++ script.js
@@ -0,0 +1,63 @@
+// Code goes here
+
+var app = angular.module("octo-poll", []).
+  config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    $routeProvider.when('/home',
+    {
+      templateUrl:    'home.html',
+      controller:     'HomeCtrl'
+    });
+    $routeProvider.when('/about',
+    {
+      templateUrl:    'about.html',
+      controller:     'AboutCtrl'
+    });
+    $routeProvider.when('/polls',
+    {
+      templateUrl:    'polls.html',
+      controller:     'PollsCtrl'
+    });
+    $routeProvider.otherwise(
+    {
+      redirectTo:     '/home',
+      controller:     'HomeCtrl', 
+    }
+  );
+});
+
+app.controller('NavCtrl', 
+['$scope', '$location', function ($scope, $location) {
+  $scope.navClass = function (page) {
+    var currentRoute = $location.path().substring(1) || 'home';
+    return page === currentRoute ? 'active' : '';
+  };
+  
+  $scope.loadHome = function () {
+        $location.url('/home');
+    };
+    
+      $scope.loadAbout = function () {
+        $location.url('/about');
+    };
+    
+      $scope.loadPolls = function () {
+        $location.url('/polls');
+    };
+    
+}]);
+
+app.controller('AboutCtrl', function($scope, $compile) {
+  console.log('inside about controller');
+
+});
+
+app.controller('HomeCtrl', function($scope, $compile) {
+  console.log('inside home controller');
+
+});
+
+app.controller('PollsCtrl', function($scope, $compile) {
+  console.log('inside poll controller');
+
+});
