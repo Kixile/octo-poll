3675288ff32a9a9bde446175e438124a0634d17d
Jarmo-Hendrik Hints <malbidion@gmail.com>
2015-04-24 21:17:44 +0300

    Trying to fix CORS by removing httpS

--- polls.js
+++ polls.js
@@ -1,5 +1,5 @@
 app.factory('polls', ['$http', function($http) {
-    return $http.get('https://octo-poll.herokuapp.com/user_questions.php?callback=JSON_CALLBACK')
+    return $http.get('http://octo-poll.herokuapp.com/user_questions.php?callback=JSON_CALLBACK')
         .success(function(data) {
             return data;
         })
