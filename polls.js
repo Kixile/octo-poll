5ab52d50d16893880d467b19b9ba5a63a3c93a34
Innar Hallik <innar.hallik@gmail.com>
2015-04-26 20:42:32 +0300

    moveing to resources

--- polls.js
+++ polls.js
@@ -1,5 +1,5 @@
 app.factory('polls', ['$http', function($http) {
-    return $http.get('https://octo-poll.herokuapp.com/user_questions.php?callback=JSON_CALLBACK')
+    return $http.get('https://octo-poll.herokuapp.com/resources/user_questions.php?callback=JSON_CALLBACK')
         .success(function(data) {
             return data;
         })
