ac47aa02ddb1eee32d089bb5fe17505083f24fa8
Innar Hallik <innar.hallik@gmail.com>
2015-04-27 00:40:02 +0300

    Revert "Testing google signin"
    This reverts commit e1251acf0070b7a54ef4e5febba41802d2ea521c.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = angular.module("octo-poll", ['directive.g+signin']).
+var app = angular.module("octo-poll", []).
   config(function ($routeProvider, $locationProvider, $httpProvider) {
 
     $routeProvider.when('/home',
@@ -42,13 +42,6 @@ app.controller('NavCtrl',
       $scope.loadPolls = function () {
         $location.url('/polls');
     };
-
-    $scope.$on('event:google-plus-signin-success', function (event,authResult) {
-    // Send login to server or save into cookie
-  });
-  $scope.$on('event:google-plus-signin-failure', function (event,authResult) {
-    // Auth failure or signout detected
-  });
     
 }]);
 
