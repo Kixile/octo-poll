9f104a4e0f2f92ef616586ca391fb994d2925670
Innar Hallik <innar.hallik@gmail.com>
2015-05-24 20:33:02 +0300

    more data to json

--- resources/user_questions.php
+++ resources/user_questions.php
@@ -7,13 +7,21 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Get data from database
 $result = pg_query($pg_conn, "SELECT * FROM user_questions");
-pg_close ($pg_conn);
 $out = array();
 # Add all to a array
 while ($row = pg_fetch_row($result)) { 
-	$data = ["id" => (string)$row[0],"question" => (string)$row[1],"description" => (string)$row[2],"multioption" => (string)$row[3],"other" => (string)$row[4],"owner" => (string)$row[5] ];
+	$answers_data = pg_query($pg_conn, "SELECT * FROM answers WHERE questions_id=" . (string)$row[0]);
+	pg_close ($pg_conn);
+	$answers = array();
+	# Add all answers to a array
+	while ($row2 = pg_fetch_row($answers_data)) { 
+		$data = ["id" => (string)$row2[0],"answer" => (string)$row2[2],"count" => (string)$row2[3]];
+		array_push($answers,$data);
+	}	
+	$data = ["id" => (string)$row[0],"question" => (string)$row[1],"description" => (string)$row[2],"multioption" => (string)$row[3],"other" => (string)$row[4],"owner" => (string)$row[5] ,"answers" => $answers ];
 	array_push($out,$data);
 }
+pg_close ($pg_conn);
 # Create json
 echo json_encode(["polls" => $out]);
 ?>
