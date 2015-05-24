2a1fcf603d63269c260c55054c05c48c626d372c
Innar Hallik <innar.hallik@gmail.com>
2015-05-24 23:49:04 +0300

    /polls/<id> gets actual id

--- PollIdController.js
+++ PollIdController.js
@@ -1,5 +1,10 @@
 app.controller('PollIdController', ['$scope', 'polls', '$routeParams', function($scope, polls, $routeParams) {
-  polls.success(function(data) {
-    $scope.detail = allPolls.polls[$routeParams.id];
+  polls.success(function(data) {	
+	id = $routeParams.id;
+	for(var k in data.polls) {
+		if (data.polls[k].id == id) {
+			$scope.detail = data.polls[k];
+		}
+	}	
   });
 }]);
